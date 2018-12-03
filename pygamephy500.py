81bdb3af64f02ef5319da6fa422b6ad3064fa00f
Jay <j.coleman@digipen.edu>
2018-12-03 12:55:57 -0800

    redid integration

--- pygamephy500.py
+++ pygamephy500.py
@@ -52,37 +52,6 @@ class Tube:
     def update(self, x1, y1, x2, y2):
         self.startpos = (x1, y1)
         self.endpos = (x2, y2)
-        
-
-def rungekuttasecondorder(f1, f2, deltaTime, alpha):
-    h = deltaTime
-    t = 0
-    x = alpha[0]
-    y = alpha[1]
-    i = 1
-    xs = []
-    ys = []
-    ts = []
-    xs.append(x)
-    ys.append(y)
-    ts.append(t)
-    k1 = [0, 0]
-    k2 = [0, 0]
-    k3 = [0, 0]
-    k4 = [0, 0]
-    k1[0] =  f1(t, x, y)
-    k1[1] =  f2(t, x, y)
-    k2[0] =  f1(t + h/2, x + k1[0]/2, y + k1[1]/2)
-    k2[1] =  f2(t + h/2, x + k1[0]/2, y + k1[1]/2)
-    k3[0] =  f1(t + h/2, x + k2[0]/2, y + k2[1]/2)
-    k3[1] =  f2(t + h/2, x + k2[0]/2, y + k2[1]/2)
-    k4[0] =  f1(t + h, x + k3[0], y + k3[1])
-    k4[1] =  f2(t + h, x + k3[0], y + k3[1])
-    x = x + h * (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0])/6
-    y = y + h * (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1])/6
-    t = 0 + i * h
-    i = i + 1
-    return y
 
 def main():
     # initialize pygame
@@ -96,14 +65,16 @@ def main():
     #get input
     print('Input mass 1:')
     m1 = input()
+    m1 = float(m1)
     print('Input line 1:')
     l1 = input()
-    l1 = int(l1)
+    l1 = float(l1)
     print('Input mass 2:')
     m2 = input()
+    m2 = float(m2)
     print('Input line 2:')
     l2 = input()
-    l2 = int(l2)
+    l2 = float(l2)
     print('Input theta 1 angle:')
     theta1 = input()
     theta1 = float(theta1) * (np.pi/180)
@@ -112,19 +83,14 @@ def main():
     theta2 = float(theta2) * (np.pi/180)
     print('Input g:')
     g = input()
+    g = float(g)
     
-    
-    def velocitytheta1(t, theta1, theta2):
-        return theta1
 
-    def positiontheta1(t, theta1, theta2):
-        return (-g*(m1 + m2 + m1)*np.sin(theta1) - m2*g*np.sin(theta1 - 2*theta2) - 2*np.sin(theta1 -theta2)*m2*(theta1**2*l1*np.cos(theta1 - theta2)+theta2**2 *l2)) / (l1(m1 + m2 +m1 - m2*np.cos(2*theta1 - 2*theta2)))
+    def positiontheta1(theta1, theta2, dtheta1, dtheta2):
+        return (-np.sin(theta1 - theta2)*(m2*l1*dtheta1**2*np.cos(theta1 -theta2) + m2*l2*dtheta2**2) - g*((m1 + m2)*np.sin(theta1)- m2*np.sin(theta2)* np.cos(theta1 - theta2))) / l1*(m1 + m2*np.sin(theta1 - theta2)**2) *0.001
 
-    def velocitytheta2(t, theta1, theta2):
-        return theta2
-
-    def positiontheta2(t, theta1, theta2):
-        return (2*np.sin(theta1 - theta2)*((m1 + m2)* theta1**2 * l1 + g*(m1+m2)*np.cos(theta1) + theta2**2 * l2*m2*np.cos(theta1 - theta2))) / l2*((m1 + m2) + m1 - m2*np.cos(2*theta1 - 2*theta2))
+    def positiontheta2(theta2, theta1, dtheta1, dtheta2):
+        return (np.sin(theta1 - theta2)*((m1 + m2)*l1*dtheta1**2*np.cos(theta1 - theta2)) + g*((m1 + m2)*np.sin(theta1)*np.cos(theta1 -theta2) - (m1 + m2)*np.sin(theta2))) / l2*(m1 + m2*np.sin(theta1 - theta2)**2) *0.001
 
     
     
@@ -134,23 +100,27 @@ def main():
     #font
     msg = pygame.font.Font('freesansbold.ttf',10)
     
-    x1 = l1*np.sin(theta1)
-    y1 = l1*np.cos(theta1)
+    x1 = int(l1*np.sin(theta1))
+    y1 = int(l1*np.cos(theta1))
     
-    x2 = x1 + l2*np.sin(theta2)
-    y2 = y1 + l2*np.cos(theta2)
+    x2 = x1 + int(l2*np.sin(theta2))
+    y2 = y1 + int(l2*np.cos(theta2))
     
     mass = Mass(int(x1), int(y1), 10, red)
     mass2 = Mass(int(x2), int(y2), 10, blue)
     
-    tube1 = Tube((240 , 0), (mass2.x, mass2.y), black)
-    tube2 = Tube((mass2.x, mass2.y), (mass.x, mass.y), green)
+    tube1 = Tube((240 , 0), (mass.x, mass.y), black)
+    tube2 = Tube((mass.x, mass.y), (mass2.x, mass2.y), green)
     
     ltext1 = msg.render("Length 1 = "+ str(int(l1)), 1, black)
+    ltext2 = msg.render("Length 2 = "+ str(int(l2)), 1, black)
     
     fps = 60
     
     ticksLastFrame = 0
+    
+    dtheta1 = 0
+    dtheta2 = 0
     
     #main loop
     while running:
@@ -173,13 +143,37 @@ def main():
         deltaText = msg.render("delta time is "+ str(deltaTime), 1, black)
         
         #call integration here
+        theta1 += dtheta1
+        theta2 += dtheta2
+        rk11 = positiontheta1(theta1, theta2, dtheta1, dtheta2)
+        rk21 = positiontheta1(theta1 + rk11/2, theta2 + rk11/2, dtheta1 + rk11/2, dtheta2 + rk11/2)
+        rk31 = positiontheta1(theta1 + rk21/2, theta2 + rk21/2, dtheta1 + rk21/2, dtheta2 + rk21/2)
+        rk41 = positiontheta1(theta1 + rk31, theta2 + rk31, dtheta1 + rk31, dtheta2 + rk31)
+        
+        rk12 = positiontheta1(theta1, theta2, dtheta1, dtheta2)
+        rk22 = positiontheta1(theta1 + rk12/2, theta2 + rk12/2, dtheta1 + rk12/2, dtheta2 + rk12/2)
+        rk32 = positiontheta1(theta1 + rk22/2, theta2 + rk22/2, dtheta1 + rk22/2, dtheta2 + rk22/2)
+        rk42 = positiontheta1(theta1 + rk32, theta2 + rk32, dtheta1 + rk32, dtheta2 + rk32)
+        
+        dtheta1 += (rk11 + 2*rk21 + 2*rk31 + rk41)/6
+        dtheta2 += (rk12 + 2*rk22 + 2*rk32 + rk42)/6
+        
+        #updating values
+        x1 = l1*np.sin(theta1)
+        y1 = l1*np.cos(theta1) 
+        x2 = x1 + l2*np.sin(theta2)
+        y2 = y1 + l2*np.cos(theta2)
                 
+        #update positions here
+        mass.update(int(x1), int(y1))
+        mass2.update(int(x2), int(y2))
         tube1.update(240, 0, mass2.x, mass2.y)
         tube2.update(mass2.x, mass2.y, mass.x, mass.y)
         
         #Render Code that will not change
         screen.blit(ltext1, (0, 10))
-        screen.blit(deltaText, (0, 20))
+        screen.blit(ltext2, (0, 20))
+        screen.blit(deltaText, (0, 30))
         mass.display()
         mass2.display()
         tube1.display()
